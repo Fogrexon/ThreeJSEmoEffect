a5470f92289635731661eec935d85c56512cd3ea
Fogrexon <fogrexon@gmail.com>
2020-09-24 17:44:51 +0900

    initial

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => (
+  <ul>
+    <li><Link to="/test1">test1</Link></li>
+  </ul>
+);
+
+export default Home;
